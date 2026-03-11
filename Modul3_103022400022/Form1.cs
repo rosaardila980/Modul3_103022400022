6408d922799d4a87e71b1f93299f3f81275fabdf
rosaardila980 <rosaardila980@gmail.com>
2026-03-11 08:21:13 +0700

    Menambahkan implementasi konversi suhu

--- Modul3_103022400022/Form1.cs
+++ Modul3_103022400022/Form1.cs
@@ -6,5 +6,44 @@ namespace Modul3_103022400022
         {
             InitializeComponent();
         }
+
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (cmbSatuanAwal.SelectedItem == null || cmbSatuanAkhir.SelectedItem == null)
+            {
+                MessageBox.Show("Pilih satuan Terlebih dahulu!");
+                return;
+            }
+            if (!double.TryParse(txtNilaiAwal.Text, out double nilaiAwal))
+            {
+                MessageBox.Show("Masukan Angka yang valid!");
+                return;
+            }
+            string satuanAwal = cmbSatuanAwal.SelectedItem.ToString();
+            string satuanAkhir = cmbSatuanAkhir.SelectedItem.ToString();
+            double celsius;
+            switch (satuanAwal)
+            {
+                case "Celsius": celsius = nilaiAwal; break;
+                case "Fahrenheit": celsius = (nilaiAwal - 32) * 5 / 9; break;
+                case "Kelvin": celsius = nilaiAwal - 273.15; break;
+                case "Reamur": celsius = nilaiAwal * 5 / 4; break;
+               
+
+
+            }
+            double hasil;
+            switch (satuanAkhir)
+            {
+                case "Celsius": hasil = celsius; break;
+                case "Fahrenheit": hasil = (celsius - 32) * 5 / 9; break;
+                case "Kelvin": hasil = celsius - 273.15; break;
+                case "Reamur": hasil = celsius * 5 / 4; break;
+                default: hasil = celsius; break;
+
+            }
+            txtNilaiAkhir.Text = hasil.ToString("F2");
+        }
     }
 }
